30a928be638c3d61e9104d4bae888e48b05cffd9
kingzez <kingzez@sina.com>
2021-06-22 10:58:05 +0800

    feat: add naive-ui

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,15 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  <NButton>开关</NButton>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import HelloWorld from './components/HelloWorld.vue'
+import { NButton } from 'naive-ui'
 
 export default defineComponent({
   name: 'App',
   components: {
-    HelloWorld
+    NButton
   }
 })
 </script>
