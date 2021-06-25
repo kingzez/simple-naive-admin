d69c3e82a344de54ae8f4967703e79b297b90187
kingzez <kingzez@sina.com>
2021-06-25 18:16:31 +0800

    chore: some refactor

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,8 @@ import App from './App.vue'
 import { router } from '@/router'
 import { store } from '@/store'
 
-// 等宽字体
-import 'vfonts/FiraCode.css'
+import 'modern-normalize/modern-normalize.css' // normalize browsers' default style
+import 'vfonts/FiraCode.css' // 等宽字体
 import '@/styles/index.scss' // global css
 
 createApp(App).use(router).use(store).mount('#app')
